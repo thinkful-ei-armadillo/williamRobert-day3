898d0068ebacfdc7529c5dbb56adc974e02c0c1a
William Wong <wwong1987@gmail.com>
2019-01-16 08:29:40 -0800

    first 2 drill problems

---
+++ app.js
@@ -0,0 +1,24 @@
+function createMyObject() {
+    return {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: function() {
+            return 'hello'
+        },
+    };
+}
+
+console.log(createMyObject())
+const obj = createMyObject();
+
+function updateObject(obj) {
+    obj.foo = 'foo',
+    obj.bar = 'bar',
+    obj.bizz = 'bizz',
+    obj.bang = 'bang'
+    return obj;
+}
+console.log(updateObject(obj));
+
+
