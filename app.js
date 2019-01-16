d412799b70fd3bcae310970269f78e3a014db09a
bobnearents <bobnearents@yahoo.com>
2019-01-16 08:43:13 -0800

    finished object drills 1

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
-function createMyObject() {
-    return {
-        foo: 'bar',
-        answerToUniverse: 42,
-        'olly olly': 'oxen free',
-        sayHello: function() {
-            return 'hello'
-        },
-    };
-}
+'use strict';
 
-console.log(createMyObject())
+//Object Drills 1   
+//Object Creator
+
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'hello';
+    },
+  };
+}
 const obj = createMyObject();
 
+//Object Updater
+
 function updateObject(obj) {
-    obj.foo = 'foo',
-    obj.bar = 'bar',
-    obj.bizz = 'bizz',
-    obj.bang = 'bang'
-    return obj;
+  obj.foo = 'foo',
+  obj.bar = 'bar',
+  obj.bizz = 'bizz',
+  obj.bang = 'bang';
+  return obj;
 }
-console.log(updateObject(obj));
+updateObject(obj);
+
+//Self-reference
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}
+
+// console.log(personMaker().fullName());
+
+//Deleting Keys
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+
+  return obj;
+}
+
+keyDeleter(obj);
 
 
