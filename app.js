fd23f2ea4ec1ef3fbd736991a63a4650935c8bf3
bobnearents <bobnearents@yahoo.com>
2019-01-16 09:19:10 -0800

    finished object drills 2

--- app.js
+++ app.js
@@ -57,50 +57,96 @@ keyDeleter(obj);
 // Object drills 2
 
 function makeStudentReport(data) {
-    const results = [];
-    for (let i in data) {
-        const student = data[i];
-        results.push(`${student.name}: ${student.grade}`);
-    }
-    return results;
+  const results = [];
+  for (let i in data) {
+    const student = data[i];
+    results.push(`${student.name}: ${student.grade}`);
+  }
+  return results;
 }
 
 const data = [
-    { name: 'Jane Doe', grade: 'A' },
-    { name: 'John Dough', grade: 'B' },
-    { name: 'Jill Do', grade: 'A' },
-  ];
+  { name: 'Jane Doe', grade: 'A' },
+  { name: 'John Dough', grade: 'B' },
+  { name: 'Jill Do', grade: 'A' },
+];
 
 // console.log(makeStudentReport(data));
 
 const studentData = [
-    {
-      name: 'Tim',
-      status: 'Current student',
-      course: 'Biology',
-    },
-    {
-      name: 'Sue',
-      status: 'Withdrawn',
-      course: 'Mathematics',
-    },
-    {
-      name: 'Liz',
-      status: 'On leave',
-      course: 'Computer science',
-    },
-  ];
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
 
 function enrollInSummerSchool(students) {
-    const results = [];
-    for (let i in students) {
-        const student = students[i];
-        student.status = 'In Summer School';
-    }
-    return students;
+  const results = [];
+  for (let i in students) {
+    const student = students[i];
+    student.status = 'In Summer School';
+  }
+  return students;
 }
 
 // console.log(enrollInSummerSchool(studentData));
+
+function findById(items, idNum) {
+  
+  for (let i in items) {
+    const item = items[i];
+    if (item.id === idNum) {
+      return item;
+    }
+  }
+}
+/*
+const testData = [
+    { id: 1, foo: 'bar' },
+    { id: 2, foo: 'bizz' },
+    { id: 3, bang: 'boo' },
+];
+
+console.log(findById(testData, 3));
+*/
+
+
+
+function validateKeys(object, expectedKeys) {
+  let counter = 0;
+  for (let i in object) {
+    counter ++;
+  }
+
+  if (counter === expectedKeys ) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+/*
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+console.log(validateKeys(objectA, 3));
+*/
+
 
 
 
